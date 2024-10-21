5d0d987aa6702a05b5558f34dce0d9dbc02d37e4
dendy192 <dtiawan192@gmail.com>
2024-10-22 00:28:40 +0700

    first commit

---
+++ src/components/NewslatterBox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const NewslatterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-800">
+        Subscribe now & get 20% off
+      </p>
+      <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+        autem consectetur illum rerum consequuntur facilis maxime numquam,
+        dolore enim ipsam quidem, beatae fuga? Recusandae sapiente, deserunt
+        doloribus explicabo sunt reiciendis!
+      </p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-centergap-3 mx-auto my-6 border pl-3"
+      >
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your Email"
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4"
+        >
+          {" "}
+          SUBSCRIBE
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewslatterBox;
